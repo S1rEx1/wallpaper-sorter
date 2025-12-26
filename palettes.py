cdcc39629eabbdf1fc1f8f22dfe40730f324f8b2
S1rEx1 <amenhotepyous@gmail.com>
2025-12-26 17:29:12 +0300

    main pallet.ass

---
+++ palettes.py
@@ -0,0 +1,31 @@
+# HEX-codes for themes
+# better and faster to see on main/background colors
+THEMES = {
+    "gruvbox": [
+        "#282828",  # Dark bg
+        "#cc241d",  # Red
+        "#98971a",  # Green
+        "#d79921",  # Yellow
+        "#fabd2f",  # Bright Yellow
+        "#ebdbb2",  # Foreground
+    ],
+    "catppuccin_mocha": [
+        "#1e1e2e",  # Base
+        "#cba6f7",  # Mauve
+        "#89b4fa",  # Blue
+        "#a6e3a1",  # Green
+        "#f9e2af",  # Yellow
+    ],
+    "nord": [
+        "#2e3440",  # Polar Night 1
+        "#3b4252",  # Polar Night 2
+        "#88c0d0",  # Frost Blue
+        "#bf616a",  # Aurora Red
+    ],
+    "dracula": [
+        "#282a36",  # Background
+        "#bd93f9",  # Purple
+        "#ff79c6",  # Pink
+        "#50fa7b",  # Green
+    ],
+}
